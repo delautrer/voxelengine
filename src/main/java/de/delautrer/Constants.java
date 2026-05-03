c97b3cfd96e2843c2ba2b0a0a99c8b7da7c8b381
Marvin Loeffler <loeffler.marvin@gmx.net>
2026-05-03 22:30:41 +0200

    Update 0.0.3.3_alpha

--- src/main/java/de/delautrer/Constants.java
+++ src/main/java/de/delautrer/Constants.java
@@ -3,7 +3,7 @@ package de.delautrer;
 import java.io.File;
 
 public class Constants {
-    public static final String VERSION = "0.0.3.1_alpha";
+    public static final String VERSION = "0.0.3.3_alpha";
     public static final String NAMESPACE = "engine";
 
     public static final boolean IS_DEV = checkIsDev();
