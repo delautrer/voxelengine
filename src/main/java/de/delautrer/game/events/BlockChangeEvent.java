2db13f887e406bd1110561b825c101874a10dd55
Marvin Loeffler <loeffler.marvin@gmx.net>
2026-09-06 16:08:14 +0200

    Mondtexture gefixxt. Performance Sachen gemacht und so RC1-0.0.4.5_alpha?

--- src/main/java/de/delautrer/game/events/BlockChangeEvent.java
+++ src/main/java/de/delautrer/game/events/BlockChangeEvent.java
@@ -10,11 +10,17 @@ public class BlockChangeEvent implements Event {
     public final Block oldBlock;
     public final Block newBlock;
     public final Chunk chunk;
+    public final boolean playSound;
 
-    public BlockChangeEvent(Vector3i pos, Block oldBlock, Block newBlock, Chunk chunk) {
+    public BlockChangeEvent(Vector3i pos, Block oldBlock, Block newBlock, Chunk chunk, boolean playSound) {
         this.pos = pos;
         this.oldBlock = oldBlock;
         this.newBlock = newBlock;
         this.chunk = chunk;
+        this.playSound = playSound;
+    }
+
+    public BlockChangeEvent(Vector3i pos, Block oldBlock, Block newBlock, Chunk chunk) {
+        this(pos, oldBlock, newBlock, chunk, true);
     }
 }
